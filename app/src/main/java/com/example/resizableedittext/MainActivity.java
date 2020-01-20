a6c20390d578a3bb95560bc05436fa6524c33e68
sukhbeer <sukhbeer940@gmail.com>
2020-01-20 16:21:47 +0530

    commit

---
+++ app/src/main/java/com/example/resizableedittext/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.resizableedittext;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edt;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
